35111cc5c9a997acf93e973794f84a38ba5a0137
raucheil <lele.colpo@gmail.com>
2018-01-26 09:29:45 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var cleanCss = require('gulp-clean-css');
 var rev = require('gulp-rev');
 var del = require('del');
 var revReplace = require("gulp-rev-replace");
-var sass = require('gulp-sass');
+// var sass = require('gulp-sass');
  
 gulp.task('clean-js', function () {
 	return del([
@@ -31,24 +31,24 @@ gulp.task('sass',['clean-css'], function () {
       .pipe(gulp.dest('./css'));
 });
 
-gulp.task('pack-js', ['clean-js'], function () {
-    return gulp.src(['assets/js/*.js'])
-        .pipe(concat('bundle.js'))
-        .pipe(minify({
-            ext:{
-                min:'.js'
-            },
-            noSource: true
-        }))
-        .pipe(rev())
-        .pipe(gulp.dest('build/js'))
-        .pipe(rev.manifest('build/rev-manifest.json', {
-            merge: true
-        }))
-        .pipe(gulp.dest(''));
-    });
+// gulp.task('pack-js', ['clean-js'], function () {
+//     return gulp.src(['assets/js/*.js'])
+//         .pipe(concat('bundle.js'))
+//         .pipe(minify({
+//             ext:{
+//                 min:'.js'
+//             },
+//             noSource: true
+//         }))
+//         .pipe(rev())
+//         .pipe(gulp.dest('build/js'))
+//         .pipe(rev.manifest('build/rev-manifest.json', {
+//             merge: true
+//         }))
+//         .pipe(gulp.dest(''));
+//     });
  
-gulp.task('pack-css', ['sass'], function () {	
+gulp.task('pack-css', ['clean-css'], function () {	
     return gulp.src(['assets/css/alertify.css', 'assets/css/themes/default.css','assets/css/custom.css'])
         .pipe(concat('stylesheet.css'))
         .pipe(cleanCss())
